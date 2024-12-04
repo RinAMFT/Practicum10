9a5526d3217a245d144b7767a2720d8b00a3fcbf
RinAMFT <mucurkaarina@gmail.com>
2024-12-04 10:08:01 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        //Task4
+        public static int Max(int x, int y, int z)
+        {
+            return Math.Max(x, Math.Max(y, z));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("z = ");
+            int z = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Max(x, y, z));
+            Console.ReadKey();
+        }
+
+    }
+}
