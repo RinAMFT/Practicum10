a6eee1b9a5b66dc3631fdb6baf312fa87fda9ecd
RinAMFT <mucurkaarina@gmail.com>
2024-12-04 10:01:57 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static double Function(int a)
+        {
+            return (Math.Sqrt(a) + a / 2);
+        }
+             static void Main(string[] args)
+           {
+            //Task1
+            Console.WriteLine($"x = {Function(5) + Function(12) + Function(19)}");
+            Console.ReadKey();
+            }
+        }
+    }
